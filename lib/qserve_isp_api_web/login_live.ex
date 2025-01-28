8cf2d44c514358a9e16a58bd906b4b5e51cc8918
Brian Cheruiyot <bryankibet@gmail.com>
2025-01-28 12:03:33 +0300

    s

--- lib/qserve_isp_api_web/login_live.ex
+++ lib/qserve_isp_api_web/login_live.ex
@@ -2,20 +2,36 @@ defmodule QserveIspApiWeb.LoginLive do
   use QserveIspApiWeb, :live_view
 
   alias QserveIspApi.Packages
-  alias QserveIspApiWeb.Router.Helpers, as: Routes
-
 
   def mount(params, _session, socket) do
     packages = Packages.list_packages_for_nas_ip(params["nas_ipaddress"])
     mac = params["mac"]
     user_details = Packages.get_user_details(mac)
-    {:ok, assign(socket, packages: packages, user_details: user_details)}
+
+    if user_details[:active] do
+      {:ok, push_redirect(socket, to: ~p"/dashboard/#{params["username"]}/#{params["nas_ipaddress"]}/#{mac}")}
+    else
+      {:ok, assign(socket, packages: packages, user_details: user_details)}
+    end
   end
 
   def handle_event("select_package", %{"package" => package}, socket) do
-    # Redirect to Make Payment page with package details
-    {:noreply, push_redirect(socket, to: Routes.live_path(socket, QserveIspApiWeb.MakePaymentLive, package: package))}
+    {:noreply, push_redirect(socket, to: ~p"/make_payment/#{socket.assigns.user_details.name}/#{socket.assigns.user_details.nas_ipaddress}/#{socket.assigns.user_details.mac}?package=#{package}")}
   end
+
+  # def mount(params, _session, socket) do
+  #   packages = Packages.list_packages_for_nas_ip(params["nas_ipaddress"])
+  #   mac = params["mac"]
+  #   user_details = Packages.get_user_details(mac)
+  #   {:ok, assign(socket, packages: packages, user_details: user_details)}
+  # end
+
+  # def handle_event("select_package", %{"package" => package}, socket) do
+  #   # Redirect to Make Payment page with package details
+  #   {:noreply, push_redirect(socket, to: Routes.live_path(socket, QserveIspApiWeb.MakePaymentLive, package: package))}
+  #   {:ok, push_redirect(socket, to: ~p"/dashboard/#{params["username"]}/#{params["nas_ipaddress"]}/#{mac}")}
+
+  # end
 
   def render(assigns) do
     ~H"""
