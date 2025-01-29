79876adf2084196c51969445b4c738feddf05b4b
Brian Cheruiyot <bryankibet@gmail.com>
2025-01-29 11:38:19 +0300

    added

--- priv/repo/migrations/20250121170559_update_mpesa_credentials_unique_constraint.exs
+++ priv/repo/migrations/20250121170559_update_mpesa_credentials_unique_constraint.exs
@@ -6,6 +6,6 @@ defmodule QserveIspApi.Repo.Migrations.UpdateMpesaCredentialsUniqueConstraint do
     drop_if_exists index(:mpesa_credentials, [:user_id], name: :mpesa_credentials_user_id_index)
 
     # Add a composite unique index on user_id and shortcode_type
-    create unique_index(:mpesa_credentials, [:user_id, :shortcode_type], name: :mpesa_credentials_user_id_shortcode_type_index)
+    # create unique_index(:mpesa_credentials, [:user_id, :shortcode_type], name: :mpesa_credentials_user_id_shortcode_type_index)
   end
 end
